75a5fcc89c5e73629cc8b62528aebb854b8110b9
guzhenhua <707016185@qq.com>
2019-09-24 00:09:47 +0800

    3種等待

--- SeleniumWeb/src/com/guzhenhua/seleniumweb/SeleniumWeb.java
+++ SeleniumWeb/src/com/guzhenhua/seleniumweb/SeleniumWeb.java
@@ -1,7 +1,12 @@
 package com.guzhenhua.seleniumweb;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumWeb {
 
@@ -18,6 +23,18 @@ public class SeleniumWeb {
 		}
 		driver.close();
 		
+		//3中不同的等待
+//强制等待
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}	
+//顯示等待(智能等待，找到就不等了，找不到在設置的時間内就一直等)
+//		new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("css locator")));
+//隱式等待（全局的等待，每次執行查找這個元素時，都會等待）
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 }
